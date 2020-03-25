b88e845740ab9cb868120503b01497871dc90bc5
ricardocaiuba <ricardo.caiuba@gmail.com>
2020-03-24 23:48:04 -0300

    Initial config - backend

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 const express = require("express");
-
 const app = express();
+app.use(express.json());
 
-app.get("/", (request, response) => {
+/**
+ * Rota / Recursos
+ */
+
+/**
+ * GET: Buscar/Listar uma informação backend
+ * POST: Criar uma informação no backend
+ * PUT: alterar uma informação no backend
+ * DELETE: Deletar uma informação no backend
+ */
+
+/**
+ * Tipos de parâmetros
+ * Query Params: Parâmetros nomeados enviado na rota após "?" (Filtros, paginação)
+ * Route Params: Parâmetros utilizados para identificar recursos
+ * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+ */
+
+app.post("/users", (request, response) => {
+  const body = request.body;
+
+  console.log(body);
+
   return response.json({
     evento: "Semana OmniStack 11.0",
     aluno: "Ricardo Rodrigues dos Santos"
